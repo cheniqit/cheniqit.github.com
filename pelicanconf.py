55b15f9e576f39aa01d84fd885995dd3e7906c73
chenqi <290433788@qq.com>
2021-02-05 17:26:55 +0800

    s

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,14 +2,11 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = u'奇百'
-SITENAME = u"奇百的博客"
-SITEURL = 'http://cheniqit.github.com'
-
-GITHUB_URL = 'https://cheniqit.github.com'
+AUTHOR = u'lizherui'
+SITENAME = u"lizherui的程序世界"
+SITEURL = 'http://www.lizherui.com'
+GITHUB_URL = 'https://github.com/lizherui'
 ARCHIVES_URL = 'archives.html'
-ARTICLE_URL = 'pages/{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
-ARTICLE_SAVE_AS = 'pages/{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
 
 RELATIVE_URLS = True
 DEFAULT_PAGINATION = 5
@@ -22,16 +19,16 @@ DEFAULT_LANG = u'zh'
 
 DEFAULT_DATE_FORMAT = '%Y-%m-%d'
 
-GOOGLE_ANALYTICS = ''
+GOOGLE_ANALYTICS = 'UA-42648273-1'
 
 GOOGLE_CUSTOM_SEARCH_SIDEBAR = '012191777864628038963:sjrtj5zxtec'
 
-DISQUS_SITENAME = 'cheniqit'
+DISQUS_SITENAME = 'lizheruisworld'
 
 FEED_RSS = 'feeds/all.rss.xml'
 CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
 
-PLUGIN_PATHS = ['C:/Users/Administrator/Git/pelican-plugins']
+PLUGIN_PATH = u"pelican-plugins"
 PLUGINS = ["sitemap"]
 
 ## 配置sitemap 插件
@@ -50,14 +47,17 @@ SITEMAP = {
 }
 
 # Blogroll
-LINKS =  (('Zhihu', 'http://www.zhihu.com/'),
+LINKS =  (('Google', 'https://www.google.com/ncr'),
           ('Python', 'http://python.org/'),
           ('Pelican', 'http://docs.getpelican.com/'),
-          ('Maxing', 'http://maxiang.info/')          
          )
 
 # Social widget
-SOCIAL = (('Github', 'https://github.com/cheniqit'),
-          ('Weibo','http://weibo.com/cheniqit'),
+SOCIAL = (('Github', 'https://github.com/lizherui'),
+          ('Twitter', 'https://twitter.com/lzrak47'),
+          ('Facebook', 'https://www.facebook.com/profile.php?id=100004875786021'),
+          ('Linkedin', 'http://www.linkedin.com/profile/view?id=232391796'),
+          ('Weibo', 'http://weibo.com/lzrm4a1'),
+          ('Zhihu', 'http://www.zhihu.com/people/li-zhe-rui'),
          )
 
